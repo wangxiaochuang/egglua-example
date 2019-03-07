efef4c6b7ea754a0f9e0077db5d48326e2b14411
jackstraw <jackstraw@wxcs-MacBook-Pro.local>
2019-03-07 20:44:55 +0800

    update

--- app/router.lua
+++ app/router.lua
@@ -1,6 +1,9 @@
 return function(app)
     local router = app.router
-    local controller = app.controller
+    
+    router:get{path='/', handler="controller.home.index"}
+    router:get{path='/news', handler="controller.news.list"}
 
-    router.get('/', controller.home.index)
+    router:resources{path="/api/posts", handler="controller.posts"}
+    router:resources{path="/api/v1/users", handler="controller.v1.users"}
 end
