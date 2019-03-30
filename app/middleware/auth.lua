f7b6995e14aa190bb786fbc0ffe03af0b4226697
jack <jackstrawxiaoxin@gmail.com>
2019-03-30 21:01:41 +0800

    update

--- app/middleware/auth.lua
+++ app/middleware/auth.lua
@@ -1,5 +1,7 @@
 return function(options)
     return function(ctx, next)
+        ngx.say("auth begin")
         next()
+        ngx.say("auth end")
     end
 end
--- config/config.default.lua
+++ config/config.default.lua
@@ -1,3 +1,4 @@
 return {
-    middleware = {}
+    middleware = {},
+    debug = true
 }
